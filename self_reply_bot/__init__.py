a819ffe6446e7073c15176eadd44567030742b1a
scratchyone <scratchywon@gmail.com>
2021-02-27 22:02:13 -0500

    Improve incr prediction

--- self_reply_bot/__init__.py
+++ self_reply_bot/__init__.py
@@ -51,7 +51,10 @@ async def run(ctx: commands.Context):
     l2 = await ctx.send(
         content="test test test",
     )
-    for i in range(0, 300):
+    last_at1 = {"incr": 0, "timestamp": 0}
+    last_at2 = {"incr": 0, "timestamp": 1}
+
+    while True:
         od = snowflake_to_dict(l1.id)
         nd = snowflake_to_dict(l2.id)
         diff = {
@@ -60,44 +63,54 @@ async def run(ctx: commands.Context):
             "ip": nd["ip"] - od["ip"],
             "incr": nd["incr"] - od["incr"],
         }
-        print("l1/l2: " + str(diff))
+        # print("l1/l2: " + str(diff))
         pd = {
             "timestamp": nd["timestamp"] + diff["timestamp"],
             "iw": 2,
             "ip": nd["ip"] + diff["ip"],
             "incr": nd["incr"] + diff["incr"],
         }
-        print("l2: " + str(snowflake_to_dict(l2.id)))
-        print("pd: " + str(pd))
+        pd["incr"] = round(
+            (last_at2["incr"] / (last_at2["timestamp"] - last_at1["timestamp"]))
+            * (pd["timestamp"] - last_at2["timestamp"])
+        )
+        # print("l2: " + str(snowflake_to_dict(l2.id)))
+        # print("pd: " + str(pd))
         pred_id = dict_to_snowflake(pd)
         l1 = l2
         l2 = await ctx.send(
             content=f"https://discord.com/channels/{ctx.guild.id}/{ctx.channel.id}/{pred_id}"
         )
-        if l2.id == pred_id:
-            l2 = await ctx.send(
-                reference=discord.MessageReference(
-                    message_id=ctx.message.id, channel_id=ctx.channel.id
-                ),
-                content="IT WORKS THE PREVIOUS UNEDITED MESSAGE LINKS TO ITSELF",
-            )
-            break
-        else:
-            nd = snowflake_to_dict(l2.id)
-            od = snowflake_to_dict(pred_id)
-            print(l2.id)
-            print(pred_id)
-            print(
-                "l2/pred: "
-                + str(
-                    {
-                        "timestamp": nd["timestamp"] - od["timestamp"],
-                        "iw": nd["iw"] - od["iw"],
-                        "ip": nd["ip"] - od["ip"],
-                        "incr": nd["incr"] - od["incr"],
-                    }
+        if snowflake_to_dict(l2.id)["iw"] == 2:
+            last_at1 = last_at2
+            last_at2 = {
+                "incr": snowflake_to_dict(l2.id)["incr"],
+                "timestamp": snowflake_to_dict(l2.id)["timestamp"],
+            }
+            if l2.id == pred_id:
+                l2 = await ctx.send(
+                    reference=discord.MessageReference(
+                        message_id=ctx.message.id, channel_id=ctx.channel.id
+                    ),
+                    content="IT WORKS THE PREVIOUS UNEDITED MESSAGE LINKS TO ITSELF <@234020040830091265>",
                 )
-            )
+                break
+            else:
+                nd = snowflake_to_dict(l2.id)
+                od = snowflake_to_dict(pred_id)
+                print(l2.id)
+                print(pred_id)
+                print(
+                    "l2/pred: "
+                    + str(
+                        {
+                            "timestamp": nd["timestamp"] - od["timestamp"],
+                            "iw": nd["iw"] - od["iw"],
+                            "ip": nd["ip"] - od["ip"],
+                            "incr": nd["incr"] - od["incr"],
+                        }
+                    )
+                )
 
 
 bot.run(os.getenv("TOKEN"))
